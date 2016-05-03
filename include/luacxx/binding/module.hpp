4aefb9d0411425c95b01266bfc39002ae953b629
Vaillant Etienne <vaillant.etienne@gmail.com>
2016-05-03 16:51:13 +0200

    Add impl. module with unit test

---
+++ include/luacxx/binding/module.hpp
@@ -0,0 +1,102 @@
+#ifndef LUACXX_BINDING_MODULE_HPP
+# define LUACXX_BINDING_MODULE_HPP
+
+# include <luacxx/core/type_traits.hpp>
+# include <luacxx/binding/bindable.hpp>
+
+# include <memory>
+# include <map>
+
+# include <lua.hpp>
+
+namespace luacxx
+{
+  class module : public bindable
+  {
+    public:
+      typedef std::unique_ptr<bindable> bindable_ptr_type;
+
+      inline module()
+      {
+      }
+
+      inline void add(const std::string& name, bindable_ptr_type&& bindable)
+      {
+        bindables_[name] = std::move(bindable);
+      }
+
+    protected:
+      typedef std::map<std::string, bindable_ptr_type> bindables_type;
+      bindables_type bindables_;
+  };
+
+  class sub_module : public module
+  {
+    public:
+      inline sub_module(const std::string& name)
+        : name_(name)
+      {
+      }
+
+      inline virtual void bind(state_type state) override
+      {
+        lua_newtable(state);
+        for(auto& pair : bindables_)
+        {
+          const std::string& name     = pair.first;
+          bindable_ptr_type& bindable = pair.second;
+
+          bindable->bind(state);
+          lua_setfield(state, -2, name.c_str());
+        }
+
+        luaL_getsubtable(state, LUA_REGISTRYINDEX, "_LOADED");
+        lua_pushvalue(state, -2);
+        lua_setfield(state, -2, name_.c_str());
+        lua_pop(state, 1);
+        lua_pushvalue(state, -1);
+      }
+
+    private:
+      const std::string name_;
+  };
+
+  class global_module : public module
+  {
+    public:
+      inline global_module()
+      {
+        modules_.insert(std::make_pair("", this));
+      }
+
+      inline module& get_module(const std::string &name)
+      {
+        modules_type::iterator it = modules_.find(name);
+        if(it == modules_.end())
+        {
+          std::unique_ptr<module> sub = std::make_unique<sub_module>(name);
+          it = modules_.insert(std::make_pair(name, sub.get())).first;
+          this->add(name, std::move(sub));
+        }
+        return *it->second;
+      }
+
+      inline virtual void bind(state_type state) override
+      {
+        for(auto& pair : bindables_)
+        {
+          const std::string& name     = pair.first;
+          bindable_ptr_type& bindable = pair.second;
+
+          bindable->bind(state);
+          lua_setglobal(state, name.c_str());
+        }
+      }
+
+    protected:
+      typedef std::map<std::string, module*> modules_type;
+      modules_type modules_;
+  };
+}
+
+#endif
---
+++ tests/test_module.cc
@@ -0,0 +1,63 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/unit_test_suite.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <luacxx/binding/module.hpp>
+#include <luacxx/binding/bindable.hpp>
+
+#include <luacxx/core/lookup_type.hpp>
+#include <luacxx/core/basic_type_info.hpp>
+
+#include "helper.hpp"
+#include <config.h>
+
+namespace
+{
+  class const_integer_field : public luacxx::bindable
+  {
+    public:
+      const_integer_field(int value)
+        : value_(value)
+      {
+      }
+
+      virtual void bind(luacxx::state_type state) override
+      {
+        lua_pushinteger(state, value_);
+      }
+
+    private:
+      int value_;
+  };
+}
+
+BOOST_AUTO_TEST_CASE(module_01)
+{
+  luacxx::lookup_type lookup;
+  lookup.set<bool>(std::make_shared<luacxx::bool_type_info>());
+  lookup.set<std::string>(std::make_shared<luacxx::string_type_info<std::string>>());
+  lookup.set<int>(std::make_shared<luacxx::integer_type_info<int>>());
+  lookup.set<double>(std::make_shared<luacxx::number_type_info<double>>());
+
+  {
+    lua_state_guard       state;
+    luacxx::global_module module;
+
+    luaL_openlibs(state);
+
+    std::unique_ptr<const_integer_field> c1 = std::make_unique<const_integer_field>(5);
+    std::unique_ptr<const_integer_field> c2 = std::make_unique<const_integer_field>(20);
+    std::unique_ptr<const_integer_field> c3 = std::make_unique<const_integer_field>(30);
+
+    module.get_module("").add("bla1", std::move(c1));
+    module.get_module("some.thing").add("bla2", std::move(c2));
+    module.get_module("some.thing").add("bla3", std::move(c3));
+    module.bind(state);
+
+    if(luaL_dofile(state, LUACXX_TESTS_DIR "module_01.lua") != LUA_OK)
+    {
+      BOOST_FAIL(lua_tostring(state, -1));
+    }
+  }
+}
