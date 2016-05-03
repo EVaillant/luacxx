c8760a0d598b28879730d4b0e67856be8153eec7
Vaillant Etienne <vaillant.etienne@gmail.com>
2016-05-03 16:51:13 +0200

    Add impl. module with unit test

---
+++ include/luacxx/binding/bindable.hpp
@@ -0,0 +1,28 @@
+#ifndef LUACXX_BINDING_BINDABLE_HPP
+# define LUACXX_BINDING_BINDABLE_HPP
+
+# include <luacxx/core/type_traits.hpp>
+
+namespace luacxx
+{
+  class bindable
+  {
+    public:
+      inline bindable()
+      {
+      }
+
+      inline virtual ~bindable()
+      {
+      }
+
+      bindable(const bindable&) = delete;
+      bindable(bindable&&) = delete;
+      bindable& operator=(const bindable&) = delete;
+      bindable& operator=(bindable&&) = delete;
+
+      virtual void bind(state_type state) = 0;
+  };
+}
+
+#endif
---
+++ tests/module_01.lua
@@ -0,0 +1,5 @@
+assert(bla1 == 5);
+
+local l = require("some.thing");
+assert(l.bla2 == 20);
+assert(l.bla3 == 30);
